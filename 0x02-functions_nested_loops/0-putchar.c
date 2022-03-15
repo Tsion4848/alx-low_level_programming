9b91522a7c7aa4c865989fcfaac4bcfc454a0e87
Tsion Moges <tsionmoges25@gmail.com>
2022-03-15 22:18:31 +0300

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar from a character arrey
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0 ; i < 7 ; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
