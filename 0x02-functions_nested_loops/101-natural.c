ab732231ec77636fb7e176d2652eacd8ede86538
Tsion Moges <tsionmoges25@gmail.com>
2022-03-16 03:05:38 +0300

    updated

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,9 +9,6 @@ int main(void)
 {
 	int i , sum;
 
-	sum = 0;
-	
-
 	for (i = 0 ; i < 1024 ; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
@@ -19,7 +16,7 @@ int main(void)
 			sum = sum + i;
 		}
 	}
-	printf("%d\n" , sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
