ac5be21ff133ae473a6737e8f64ee93c7635a700
Tsion Moges <tsionmoges25@gmail.com>
2022-03-17 17:54:21 +0300

    printing numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers except 2 and 4
+ *
+ * Return: numbers
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 48 ; num <= 57 ; num++)
+	{
+		if (num != 50 && num != 52)
+		{
+			_putchar(num);
+		}
+	}
+	_putchar('\n');
+}
