f77bc2c96468016bb8fd4de073a0c7dc95fcc4cd
Tsion Moges <tsionmoges25@gmail.com>
2022-03-21 22:38:38 +0300

    finding the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s - stores the string
+ *
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = *s.strlen();
+	return(len);
+}
