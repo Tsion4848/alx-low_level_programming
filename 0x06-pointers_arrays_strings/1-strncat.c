6178891ee4eb32b0b5576306dcd4ef2b54e1bcbb
Tsion Moges <tsionmoges25@gmail.com>
2022-03-23 22:30:03 +0300

    concatenating strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,23 +10,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+	char *ret = dest;
 
-	i = 0;
+	while (*dest)
+		dest++;
 
-	while (dest[i] != 0)
-	{
-		i++;
-	}
-
-	j = 0;
-
-	while (src[j] != 0 && j < n)
-	{
-		dest[i] == src[j];
-		i++;
-		j++;
-	}
-	return (dest);
+	while (*src && n--)
+		*dest++ = *src++;
+	
+	*dest = '\0';
+	return (ret);
 }
