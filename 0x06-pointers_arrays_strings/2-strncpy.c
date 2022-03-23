695a6a7cd2fe2f13901c0956dc3bfe9eecbeecbf
Tsion Moges <tsionmoges25@gmail.com>
2022-03-23 22:07:50 +0300

    copying portion of one string to another

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,6 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	
 	i = 0;
-
 	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
@@ -21,7 +20,6 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 
 	i = i;
-
 	while (i < n)
 	{
 		dest[i] = '\0';
