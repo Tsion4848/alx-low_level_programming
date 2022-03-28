3ec73595527866a3eda88139b6f4eeba0611f768
Tsion Moges <tsionmoges25@gmail.com>
2022-03-28 20:58:19 +0300

    length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,20 +10,22 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int count = 0;
 	int i = 0;
-	
-	int j;
+	int j = 0;
 
 	while (s[i] != '\0')
 	{
-		for (j = 0 ; accept[j] != '\0' ; j++)
+		while (accept[j] != '\0')
 		{
 			if (accept[j] == s[i])
 			{
 				count++;
 			}
+			j++;
 		}
 		j = 0;
+
 		i++;
+		
 		if (s[i] == ' ')
 		{
 			break;
