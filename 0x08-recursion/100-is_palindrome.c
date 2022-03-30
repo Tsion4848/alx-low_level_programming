f9cf639152d344cbf7fbc41647757b0c0d5c6e05
Tsion Moges <tsionmoges25@gmail.com>
2022-03-31 02:24:20 +0300

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,61 +1,17 @@
 #include "main.h"
 /**
- * is_palindrome -  sss
- * @r: string
- * Return: return something
+ * is_palindrome - find a palindrome string
+ * @s: string for check
+ *
+ * Return: 1 if is palindrome, otherwise 0
  */
-
-int is_palindrome(char *r)
+int is_palindrome(char *s)
 {
-
-	int i, size, add;
+	int i;
 
 	i = 0;
-	add = 0;
 
-	size = _strlen_recursion(r);
-
-	add = (size % 2 != 0) ? 2 : 1;
-
-	return (evaluate(r, i, size - 1, add));
-}
-
-/**
- * evaluate - compare
- * @i: i
- * @size: size
- * @s: string
- * @add: addition depending if n is odd or even
- * Return: return value
- */
-int evaluate(char *s, int i, int size, int add)
-{
-
-	if (i + add == size  && s[i] == s[size])
-	{
-		return (1);
-	}
-
-	else if (s[i] == s[size])
-	{
-		return (evaluate(s, i + 1, size - 1, add));
-	}
-
-	return (0);
-}
-
-/**
- * _strlen_recursion - legth of a string
- * @s: string
- * Return: return legth
- */
-
-int _strlen_recursion(char *s)
-{
-	/*Base condition*/
-	if (!*s)
+	if (*s == i)
 		return (0);
-
-	else
-		return (1 + _strlen_recursion(s + 1)); /*Sum 1*/
+	return (1);
 }
