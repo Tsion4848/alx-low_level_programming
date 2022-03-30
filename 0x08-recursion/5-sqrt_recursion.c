a969817b2713a6d5bfecd52185ee226eb8114560
Tsion Moges <tsionmoges25@gmail.com>
2022-03-31 02:11:21 +0300

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,27 @@
 #include "main.h"
 /**
+ * square_root - find the natural square root
+ * @i: number
+ * @j: incrementor
+ *
+ * Return: the number found
+ */
+int square_root(int i, int j)
+{
+	if (i * i == j)
+	{
+		return (i);
+	}
+	else if (i * i > j)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (square_root(i + 1, j));
+	}
+}
+/**
  * _sqrt_recursion - returns the square root of a number
  * @n: the number
  *
@@ -7,4 +29,7 @@
  */
 int _sqrt_recursion(int n)
 {
-	
+	int x = 1;
+
+	return (square_root(x, n));
+}
