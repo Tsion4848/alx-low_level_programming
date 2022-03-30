ae8ec274c1405b9baecdedf47167903530d0de69
Tsion Moges <tsionmoges25@gmail.com>
2022-03-31 00:25:41 +0300

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - gets the length of a string
+ * @s: the string
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
