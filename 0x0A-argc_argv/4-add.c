7bc98e701c557bf12c5551aa040843f23930859a
Tsion Moges <tsionmoges25@gmail.com>
2022-04-01 23:52:38 +0300

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,6 +13,10 @@ int main(int argc, char *argv[])
 	int i, j, sum;
 	
 	sum = 0;
+	
+	if (argc == 1){
+		printf("%s\n", 0);
+	}
 
 	for (i = 1 ; i < argc ; i++)
 	{
@@ -23,9 +27,6 @@ int main(int argc, char *argv[])
 				printf("Error\n");
 				return (1);
 			}
-			else if (argc == 1)
-			{
-				printf("%s\n", 0);
 		}
 		sum = sum + atoi(argv[i]);
 	}
