6ab8d10ead2e2915d8891fcea9fd126de7c9f728
Tsion Moges <tsionmoges25@gmail.com>
2022-04-02 02:53:23 +0300

    adding

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,22 +10,22 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, sum;
-	
-	sum = 0;
+	int i, sum = 0;
 
+	if (argc < 1)
+	{
+		return (0);
+	}
 	for (i = 1 ; i < argc ; i++)
 	{
-		for (j = 0 ; argv[i][j] != '\0' ; j++)
+		if (!atoi(argv[i]))
 		{
-			if (argv[i][j] < 47 && argv[i][j] > 57)
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("%s\n", "Error");
+			return (1);
 		}
 		sum = sum + atoi(argv[i]);
 	}
 	printf("%d\n", sum);
+
 	return (0);
 }
