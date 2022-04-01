5b40faaed37dcd945a1675059fe5ff09be482ef6
Tsion Moges <tsionmoges25@gmail.com>
2022-04-01 23:22:26 +0300

    multiplication of 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,8 +12,9 @@ int main(int argc, char *argv[])
 {
 	int result;
 
-	if (argc == 3){
-		result = atoi(argv[1] * argv[2]);
+	if (argc == 3)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", result);
 	}
 	else
