5a98d5556bdcd58bf43b038146c32c613fd5299b
Tsion Moges <tsionmoges25@gmail.com>
2022-04-11 22:50:51 +0300

    name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: the arguments
+ *
+ * Return: void
+ */
+int main(int argc, *char argv[])
+{
+	printf("the file name is %s\n", argv[0]);
+	return (0);
+}
