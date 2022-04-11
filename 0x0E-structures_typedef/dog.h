48a22581082a267aff1847e58d504e1deabaa90c
Tsion Moges <tsionmoges25@gmail.com>
2022-04-12 00:30:56 +0300

    creating a structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,11 @@
+/**
+ * struct dog - creates a struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: this describes the name, age, and owner of a dog
+ */
 struct dog
 {
 	char *name;
