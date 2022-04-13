90433a3c06922d0b9a03aa57377b466d5fe2a371
Tsion Moges <tsionmoges25@gmail.com>
2022-04-14 00:39:57 +0300

    printing a string

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,12 +5,8 @@
  * @name: the string to print
  * @f: pointer to function
  *
- * Return: function to pointer to print name
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f)
-	{
-		(*f)(name);
-	}
+	f(name);
 }
