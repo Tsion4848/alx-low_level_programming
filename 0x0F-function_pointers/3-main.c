cf21d1f2d939edc93ebb4552e270b360f65dbe26
Tsion Moges <tsionmoges25@gmail.com>
2022-04-14 00:55:25 +0300

    calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,24 +8,23 @@
  */
 int main(int argc, char *argv[])
 {
-        int op , num1, num2;
+	int op, num1, num2;
 
-        if (argc != 4)
-        {
-                printf("Error\n");
-                exit(98);
-        }
-        if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' &&
-             argv[2][0] != '%' && argv[2][0] != '*') || strlen(argv[2]) != 1)
-        {
-                printf("Error\n");
-                exit(99);
-        }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' && argv[2][0] != '%' && argv[2][0] != '*') || strlen(argv[2]) != 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-        num1 = atoi(argv[1]);
-        num2 = atoi(argv[3]);
-        op = get_op_func(argv[2])(num1, num2);
-        printf("%d\n", op);
-        return (0);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = get_op_func(argv[2])(num1, num2);
+	printf("%d\n", op);
+	return (0);
 }
 
