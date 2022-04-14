afd036d13fe0ba72ad8f0c220d69162ed3cb1b2a
Tsion Moges <tsionmoges25@gmail.com>
2022-04-14 17:54:19 +0300

    updated

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,11 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0 ; i < n ; i++)
 	{
 		str = va_arg(ap, char *);
-
-		if (str == NULL)
-		{
-			printf("%p", str)
-		}
+		
 		else
 		{
 			printf("%s", str);
