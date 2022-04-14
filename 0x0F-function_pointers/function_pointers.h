38a1641f0e51bb4ee8d0cf773be7addfd744ab10
Tsion Moges <tsionmoges25@gmail.com>
2022-04-14 18:19:56 +0300

    printing a string

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
+#ifndef FUNC_POINTER
+#define FUNC_POINTER
 #include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 int int_index(int *array, int size, int (*cmp)(int));
+#endif
