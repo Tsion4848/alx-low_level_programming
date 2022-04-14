ae9d625a7a3e29595a789f26e4f49859c2ecd986
Tsion Moges <tsionmoges25@gmail.com>
2022-04-14 17:52:50 +0300

    pritning string

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -15,3 +15,4 @@ typedef struct print_all
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
