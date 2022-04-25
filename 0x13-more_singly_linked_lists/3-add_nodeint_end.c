250bc891497f9174059c4e756360972aca52ecc0
Tsion Moges <tsionmoges25@gmail.com>
2022-04-25 22:39:22 +0300

    updated

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,10 +11,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new;
 	listint_t *p;
 
-	if (&*head == NULL)
-	{
-		return (NULL);
-	}
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
