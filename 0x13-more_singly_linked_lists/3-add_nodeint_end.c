2942c814549e43d21ab78a5695b83afd4b79d5ae
Tsion Moges <tsionmoges25@gmail.com>
2022-04-26 21:03:35 +0300

    adding at end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,13 +15,16 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		return (NULL);
 	}
+
 	new = malloc(sizeof(listint_t));
+	
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 	new->n = n;
 	new->next = NULL;
+	
 	if (*head == NULL)
 	{
 		*head = new;
