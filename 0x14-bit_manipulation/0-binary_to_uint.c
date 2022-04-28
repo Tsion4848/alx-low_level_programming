98657a8c3931dc400fcde96e8e017f9e8f10d2f8
Tsion Moges <tsionmoges25@gmail.com>
2022-04-28 23:11:29 +0300

    converting binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		base = base *2 + b[i] - '0';
+		base = base * 2 + b[i] - '0';
 	}
 	return (base);
 }
