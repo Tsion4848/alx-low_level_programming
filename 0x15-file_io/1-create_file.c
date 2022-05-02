500ad0df7b3ebf1cedb604a4b5d6e38c82f992ff
Tsion Moges <tsionmoges25@gmail.com>
2022-05-03 00:07:53 +0300

    creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,20 +8,34 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	fd = open(filename, O_RDWR, 0600);
+	int fd, i, w;
+	
 	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	
+	if (fd == -1)
 	{
 		return (-1);
 	}
 
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_CREATE)
+		text_content = "";
 	}
-	if (fd != -1)
+	for (i = 0 ; text_content[i] != '\0' ; i++)
+		;
+
+	w = write(fd, text_content, i)
+
+	if (w == -1)
 	{
-		truncate(filename, letters);
+		return (-1);
 	}
+
+	close(fd);
 	return (1);
 }
