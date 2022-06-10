90421cf2f529d3aaa195a0e47411810ff43a9c58
Tsion Moges <tsionmoges25@gmail.com>
2022-06-10 19:40:55 +0300

    adding an element at the beginning

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_dnodeint - adds a new node at the beginning
+ * @head: the head node
+ * @n: data to insert
+ *
+ * Return; address of the new element or null if failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = mallox(sizeof(dlistint_t));
+
+	if (new != NULL)
+	{
+		new->next = *head;
+		new->prev = NULL;
+		new->n = n;
+
+		if (*head)->prev = new;
+		{
+			(*head)->prev = new;
+		}
+		*head = new;
+		return (new)
+	}
+	return (NULL);
+}
