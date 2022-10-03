380ee2a749ba73769b9991e4f7cce59d0b86e6fa
Tsion Moges <tsionmoges25@gmail.com>
2022-10-03 11:47:35 +0300

    Create search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,25 @@
+#ifndef SEARCH_ALGORITHMS_H
+#define SEARCH_ALGORITHMS_H
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for ALX project
+ */
+typedef struct listint_s
+{
+		int n;
+		size_t index;
+		struct listint_s *next;
+} listint_t;
+
+int linear_search(int *array, size_t size, int value);
