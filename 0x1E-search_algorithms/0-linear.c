075c018a639f2b52af0da3b18d7f9361bdc69e79
Tsion Moges <tsionmoges25@gmail.com>
2022-10-03 11:56:43 +0300

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,17 +2,20 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-  if (array == NULL)
-  {
-    return -1;
-  }
-  int i;
-  for (i = 0 ; i < size ; i++)
-  {
-    if (array[i] == value)
-    {
-      return i;
-    }
-   return -1;
-  }
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	size_t i;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	
+	return (-1);
 }
